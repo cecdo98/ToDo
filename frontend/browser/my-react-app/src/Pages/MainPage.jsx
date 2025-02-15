730a7c342c234637a7dd0dd4f2884fd471bafab5
Carlos Oliveira <carlosemanuel98@hotmail.com>
2025-02-15 13:17:48 +0000

    react-prepare-for-php

--- frontend/browser/my-react-app/src/Pages/MainPage.jsx
+++ frontend/browser/my-react-app/src/Pages/MainPage.jsx
@@ -4,16 +4,17 @@ import Footer from "../components/Buttons/Footer.jsx"
 
 function MainPage(){
 
+  const Notes =[{ id:1, titulo: "Consulta" ,descricao :"Hospital porto" ,texto: "Consulta dia 10 de Março"},
+                { id:2, titulo: "Teste", descricao: "Ispgaya" , texto: "Prova de ingles dia 1 de Março"},
+                { id:3, titulo: "IRS", descricao : "" ,texto: "Declaração de gastos ate final de Fevereiro"}
+              ];
+
+
     return(
         <>
         <Header/>
 
-           {/*escrever um loop para cada entrada? Descobrir como fazer isso */}
-
-            <Note titulo="Consulta" descricao ="Hospital porto" texto="Consulta dia 10 de Março"/>
-            <Note titulo="Teste" descricao ="Ispgaya" texto="Prova de ingles dia 1 de Março"/>
-            <Note titulo="IRS" descricao ="" texto="Declaração de gastos ate final de Fevereiro"/>
-          
+        <Note Notes={Notes}/>  
         <Footer/>
       
       </>
--- frontend/browser/my-react-app/src/components/Buttons/note.jsx
+++ frontend/browser/my-react-app/src/components/Buttons/note.jsx
@@ -1,35 +1,32 @@
 import ButtonEdit from "./ButtonEdit";
 import ButtonDone from "./ButtonDone";
-import propTypes from "prop-types"
 
-function Note(props){
+function Note({Notes}){
 
     return(
-        <div className="note" >
-            <div className="titulo">
-                <h2>{props.titulo}</h2>
-            </div>
-            <div className="conteudo">
-            <h3 className="descricao">{props.descricao}</h3>
-            <p className="texto">{props.texto}</p>
-            </div>
-            <div className="button">
-                <div className="buttonEdit">
-                    <ButtonEdit/>
+        <div>
+            {Notes.map((note) =>(
+            <div key={note.id} className="note" >
+                <div  className="titulo">
+                    <h2>{note.titulo}</h2>
                 </div>
-                <div className="buttonDone">
-                    <ButtonDone/>
+                <div className="conteudo">
+                    <h3 className="descricao">{note.descricao}</h3>
+                    <p className="texto">{note.texto}</p>
+                </div>
+                <div className="button">
+                    <div className="buttonEdit">
+                        <ButtonEdit/>
+                    </div>
+                    <div className="buttonDone">
+                        <ButtonDone/>
+                    </div>
                 </div>
             </div>
+            ))}
         </div>
     );
-}
 
-
-Note.propTypes = {
-    titulo: propTypes.string,
-    descricao: propTypes.string,
-    texto: propTypes.string,
 }
 
 
